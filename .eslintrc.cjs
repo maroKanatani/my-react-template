d5a31b4acd70a0ebb76928ae98ab545529edacae
maroKanatani <16130443+maroKanatani@users.noreply.github.com>
2023-09-04 19:55:11 +0900

    chore: eslint整備

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,14 +8,31 @@ const config = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "simple-import-sort", "import"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "import/newline-after-import": "error",
+    "import/no-default-export": "error",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "error",
+    "react/prop-types": "off", // tsなのでoffにしておく
+    "react/no-unused-prop-types": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { prefer: "type-imports" },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
     ],
   },
 };
